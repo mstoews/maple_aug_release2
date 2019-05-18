9e3030520bbfc1c3ba93c498586ae3c3101ab75b
mst <mstoews@hotmail.com>
2019-05-19 08:24:48 +0900

    Completed ignore

---
+++ main/GoogleMapPicker/MapBoxViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapBoxViewController.swift
+//  Maple
+//
+//  Created by Murray Toews on 2019/05/08.
+//  Copyright © 2019 Murray Toews. All rights reserved.
+//
+
+import Foundation
---
+++ maple/Models/Navigation.swift
@@ -0,0 +1,9 @@
+//
+//  Navigation.swift
+//  Maple
+//
+//  Created by Murray Toews on 2019/05/10.
+//  Copyright © 2019 Murray Toews. All rights reserved.
+//
+
+import Foundation
